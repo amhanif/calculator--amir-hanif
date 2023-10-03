ef3a5bbf590b9b524f249834f96be644e4182628
amhanif <amirhanif3268@gmail.com>
2023-10-03 22:24:11 +0500

    Initial commit

---
+++ function.ts
@@ -0,0 +1,12 @@
+export function sum (a:number, b:number){
+    return a+b;
+}
+export function subtract (a:number, b:number){
+    return a-b;
+}
+export function multiply (a:number, b:number){
+    return a*b;
+}
+export function divide (a:number, b:number){
+    return a/b;
+}
